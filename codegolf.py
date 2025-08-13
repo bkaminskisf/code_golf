5c25842ba3d296a8368641cb10a6299ece25df26
Brandon Kaminski <brandon.kaminski@progress.com>
2025-08-13 11:24:22 -0400

    remove unnecessary spaces

--- codegolf.py
+++ codegolf.py
@@ -1,9 +1,9 @@
 import sys
 l = 'IVXLCDM '
-for n in [int(x) for x in open(sys.argv[1], "r").readlines()]:
+for n in [int(x) for x in open(sys.argv[1],"r").readlines()]:
 	f = ''
 	for i in [3,2,1,0]:
 		x = l[i*2]
-		v = int(n/10**i) % 10
-		f += f'{x}{l[i*2+int(v/5)+1]}' if v % 5 == 4 else l[i*2+1]*int(v/5) + x*(v % 5)
+		v = int(n/10**i)%10
+		f += f'{x}{l[i*2+int(v/5)+1]}' if v%5==4 else l[i*2+1]*int(v/5)+x*(v%5)
 	print(f)
