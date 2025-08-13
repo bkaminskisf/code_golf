49f745edb93ad3d08626aeb001700f2df3fc7b62
Brandon Kaminski <brandon.kaminski@progress.com>
2025-08-13 11:59:12 -0400

    consolidation

--- codegolf.py
+++ codegolf.py
@@ -1,9 +1,8 @@
 import sys
 l = 'IVXLCDM '
-for n in [int(x) for x in open(sys.argv[1],"r").readlines()]:
-	f = ''
-	for i in [3,2,1,0]:
-		x = l[i*2]
-		v = int(n/10**i)%10
-		f += f'{x}{l[i*2+int(v/5)+1]}' if v%5==4 else l[i*2+1]*int(v/5)+x*(v%5)
+for p in open(sys.argv[1],"r").readlines():
+	f=''
+	for i in [6,4,2,0]:
+		v=int(int(p)/10**(i/2))%10
+		f+=f'{l[i]}{l[i+int(v/5)+1]}' if v%5==4 else l[i+1]*int(v/5)+l[i]*(v%5)
 	print(f)
