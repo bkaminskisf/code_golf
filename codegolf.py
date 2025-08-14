4ce01d143ae80b219ea13b3212d56571fee92798
Brandon Kaminski <brandon.kaminski@progress.com>
2025-08-14 10:17:57 -0400

    remove int call

--- codegolf.py
+++ codegolf.py
@@ -3,6 +3,6 @@ l = 'IVXLCDM '
 for p in open(sys.argv[1],"r"):
 	f=''
 	for i in [6,4,2,0]:
-		v=int(int(p)//10**(i/2))%10
+		v=int(p)//10**(i//2)%10
 		f+=l[i]+l[i+v//5+1] if v%5==4 else l[i+1]*(v//5)+l[i]*(v%5)
 	print(f)
